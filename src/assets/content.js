c10ccce600d54e919f1fa683cb596d57c99a32d4
Kallyan01 <singhakallyan@gmail.com>
2023-06-27 17:31:06 +0530

    feat: added zigbox

---
+++ src/assets/content.js
@@ -0,0 +1,21 @@
+export const data = [{
+        image: 'https://m.economictimes.com/thumb/msid-87577205,width-1200,height-900,resizemode-4,imgsize-75004/travis-scott.jpg',
+        title: 'lorem',
+        description: 'Some description here',
+        extraButton: {
+            name: 'Learn More',
+            link: '#'
+        },
+        isCLub: true
+    },
+    {
+        image: 'https://media.npr.org/assets/img/2017/12/21/_harlan_nprmusic10years-149_wide-78a7596027f7b4c023a21ad85f078e8a4165c230-s800-c85.jpg',
+        title: 'lorem',
+        description: 'Some description here',
+        extraButton: {
+            name: 'Learn More',
+            link: '#'
+        },
+        isCLub: true
+    }
+]
